ef9cfe6226b303850cacd2323fcde681b8864de8
willconstable <will_constable@msn.com>
2020-09-18 17:31:59 +0100

    First commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link} from "gatsby";
+import Header from "../components/header";
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="About Gatsby"/>
+      <Header headerText="Its pretty cool"/>
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
